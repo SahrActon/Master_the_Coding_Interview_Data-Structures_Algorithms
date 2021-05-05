66b9a6c5e90fd81ccdbdd1d05318e8604d08e76b
Thomas Acton <tommy-acton@hormail.co.uk>
2021-05-05 09:32:31 +0100

    pushing current work!

--- Section 7: Data Structure: Hash Tables/exercise: first recurring character.js
+++ Section 7: Data Structure: Hash Tables/exercise: first recurring character.js
@@ -17,7 +17,7 @@ function findFirstRecurring_1(array1) {
         for (let j = i + 1; j < array1.length; j++) {
             if (array1[i] === array1[j]) {
                 state = true;
-                console.log(state, array1[i])
+                console.log("Algorithm 1:", state, array1[i])
                 return;
             }
         }
@@ -26,23 +26,45 @@ function findFirstRecurring_1(array1) {
     return state;
 }
 
+let finalMap = {};
+
 function findFirstRecurring_2(input) {
-    let map = {};
+
+    let map = {}; // this map will store the values as keys
     for (let i = 0; i < input.length; i++) {
-        if (map[input[i]] !== 0) {
-            console.log(input[i])
+        if (map[input[i]] !== undefined) {
+            console.log("Algorithm 2:", input[i])
             return input[i];
         } else {
             map[input[i]] = i;
         }
-        console.log(map)
+        // console.log(map)
     }
+    console.log("Nothing was found");
+    finalMap = map;
     return undefined;
 }
 
-let arrayX = [2, 5, 1, 2, 3, 5, 1, 2, 4];
-let arrayY = [0, 1, 1, 2, 3, 5, 1, 2, 4];
-let arrayZ = [2, 3, 4, 5];
+function findFirstRecurring_3(array1) {
+    // O(n^2) - because of the nested loop
+    let state = false;
+
+    for (let i = 0; i < array1.length; i++) {
+        let j = 1;
+        if (array1[i] === array1[j]) {
+            state = true;
+            console.log("Algorithm 3:", state, array1[i])
+            return;
+        }
+
+    }
+    console.log("We didnt find any matching char")
+    return state;
+}
+
+let arrayX = [2, 5, 5, 2, 3, 5, 1, 2, 4];
+let arrayY = [0, 1, 1, 2, 3, 0, 1, 2, 4];
+let arrayZ = [2, 3, 4, 'bob'];
 
 // findFirstRecurring_1(arrayX);
 // console.log("---");
@@ -51,9 +73,32 @@ let arrayZ = [2, 3, 4, 5];
 // findFirstRecurring_1(arrayZ);
 // console.log("v2\n\n");
 
+findFirstRecurring_1(arrayX);
+console.log("---");
+
+
 findFirstRecurring_2(arrayX);
 console.log("---");
+
+findFirstRecurring_3(arrayX);
+console.log("---\n\n");
+
+
+findFirstRecurring_1(arrayY);
+console.log("---");
+
+
+findFirstRecurring_2(arrayY);
+console.log("---");
+
+findFirstRecurring_3(arrayY);
+console.log("---\n\n");
+
+
 findFirstRecurring_2(arrayY);
 console.log("---");
 findFirstRecurring_2(arrayZ);
 console.log("---");
+
+/* Printing out the keys*/
+console.log(finalMap)
